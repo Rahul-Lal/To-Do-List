ec5db8607fcbd62f56e4e8c8bad4003142a03611
Rahul Lal <Rahul21Lal@gmail.com>
2019-04-26 15:48:07 +1200

    Updating JS code and UI

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Variables
+var array = ["One batch", "Two batch", "Penny & Dime"];
+
+// Functions
+console.log("WWWWWWWWWWWWWWWWWWWWWWWAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAASSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+console.log("At this moment, I writing this while listening to Joe Rogan with Kevin Smith");
+array.push("Taco");
+array.push("Put... the bunny... back... in the box.");
+console.log(array);
+
+/*
+    - Add Value: array.push("");
+    - Remove Value: array.splice(array designation no, how many delete from no);
+    - 
+*/
